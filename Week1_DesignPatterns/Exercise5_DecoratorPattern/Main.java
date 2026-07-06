aa098eb88c98ba508f4c65b7af2c455dd7da3ece
Sumaiyah Fatmee <sumaiyafatmee1957@gmail.com>
2026-07-07 01:09:53 +0530

    Completed Exercise 5 Decorator Pattern

---
+++ Week1_DesignPatterns/Exercise5_DecoratorPattern/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Notifier notifier = new SlackNotifierDecorator(
+                                new SMSNotifierDecorator(
+                                    new EmailNotifier()));
+
+        notifier.send("Your order has been shipped.");
+    }
+}
