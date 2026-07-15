3d273c34157f0fa2addcd5b69b38b46b161ebd7a
Sumaiyah Fatmee <sumaiyafatmee1957@gmail.com>
2026-07-16 00:22:02 +0530

    Completed Week 6 JWT Authentication and Authorization

---
+++ Week6_Microservices/JwtAuthAPI/Controllers/AuthController.cs
@@ -0,0 +1,63 @@
+using JwtAuthAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtAuthAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginModel model)
+        {
+            if (model.Username == "admin" && model.Password == "password")
+            {
+                var token = GenerateJwtToken(model.Username);
+
+                return Ok(new
+                {
+                    Token = token
+                });
+            }
+
+            return Unauthorized("Invalid username or password.");
+        }
+
+        private string GenerateJwtToken(string username)
+        {
+           var claims = new[]
+{
+    new Claim(ClaimTypes.Name, username),
+    new Claim(ClaimTypes.Role, "Admin")
+};
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+
+            var creds = new SigningCredentials(
+                key,
+                SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(
+                    Convert.ToDouble(_configuration["Jwt:DurationInMinutes"])),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ Week6_Microservices/JwtAuthAPI/Models/User.cs
@@ -0,0 +1,11 @@
+namespace JwtAuthAPI.Models
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
+        public string Role { get; set; } = "User";
+    }
+}
